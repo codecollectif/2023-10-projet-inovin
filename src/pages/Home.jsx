4de15c4c5a7ef01d646bce765c33b77ef936f258
jojo051 <youpi051@yahoo.fr>
2023-10-19 12:04:12 +0200

    create function getdata

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,13 @@
+import DataCepage from "../components/DataCepage";
 import Navbar from "../components/Navbar";
 
 function Home() {
+  
   return (
     <div>
       <Navbar />
       <h1>hello home</h1>
+      <DataCepage />
     </div>
   );
 }
