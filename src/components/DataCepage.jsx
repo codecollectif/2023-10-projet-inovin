5c08905bd3a40e8911a600d40a16e76a40ebca30
jojo051 <youpi051@yahoo.fr>
2023-10-19 19:30:43 +0200

    refector stylecss

--- src/components/DataCepage.jsx
+++ src/components/DataCepage.jsx
@@ -1,28 +1,28 @@
 import { useEffect, useState } from "react";
 import Papa from "papaparse";
 import Glasses from "../pictures/glass.png";
+import "../pages/home.css";
 
 const DataCepage = () => {
   const URL = "https://docs.google.com/spreadsheets/d/1Q2L2mJr6xuxMMcktiQ2i8oDr68ag_bVjXLwdUx4CKCM/export?exportFormat=csv&format=csv?pli=1#gid=0";
-  const[data,setData]=useState(["initial"]);
+  const[data,setData]=useState(["chargement"]);
 
   useEffect(()=>{
     Papa.parse(URL, {
       download: true,
       complete: function(result){
         setData(result.data.flat())
-        return console.log("All done!",result);
       }
     });
   },[])
 
   return (
     <>
-    {data.map((o,i)=>
-      <card key={i.id} className="card" >
+    {data.map((o)=>
+      <div key={o} className="card" >
         <h2 className="card-title" >{o}</h2>
         <img className="card-img" src={Glasses} alt="verre" />
-      </card>
+      </div>
     )}
     </>
   );
