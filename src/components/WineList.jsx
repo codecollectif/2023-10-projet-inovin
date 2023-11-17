2f4643b41dde527ecfff7617239e0f2bbf108c37
romaing <rocambille@gmail.com>
2023-11-17 16:43:52 +0100

    simplified conditions in WineList

--- src/components/WineList.jsx
+++ src/components/WineList.jsx
@@ -12,24 +12,13 @@ const WineList = ({ data }) => {
 
   return data.map((o) => (
     <Link
-      className={
-        JSON.stringify(Array.of(o)) ===
-        JSON.stringify(listWineName.filter((wine) => wine === o))
-          ? "card-link disable"
-          : "card-link"
-      }
+      className={listWineName.includes(o) ? "card-link disable" : "card-link"}
       key={o}
-      to={
-        JSON.stringify(Array.of(o)) ===
-        JSON.stringify(listWineName.filter((wine) => wine === o))
-          ? null
-          : `/${o}/visuel`
-      }
+      to={listWineName.includes(o) ? null : `/${o}/visuel`}
     >
       <section className="card">
         <h2 className="card-title">{o}</h2>
-        {JSON.stringify(Array.of(o)) ===
-        JSON.stringify(listWineName.filter((wine) => wine === o)) ? (
+        {listWineName.includes(o) ? (
           <img className="card-img" src={GlassWine} alt="déguster" />
         ) : (
           <img className="card-img" src={Glasses} alt="à déguster" />
